9ac343fb06d7197a3b352c7cf55d37a7d5d2ba8a
Tom <morhendos@gmail.com>
2025-06-25 12:28:42 +0200

    layout fix

--- app/rules/layout.js
+++ app/rules/layout.js
@@ -2,3 +2,7 @@ export const metadata = {
   title: 'Reglamento - Liga de Tenis del Parque',
   description: 'Reglamento oficial de la Liga de Tenis del Parque en Sotogrande. Sistema suizo, wild cards, y todo lo que necesitas saber.',
 }
+
+export default function RulesLayout({ children }) {
+  return children
+}
