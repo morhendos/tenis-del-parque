39c845d81893fb3c4f365390dd77ffa2327949cd
Tom <morhendos@gmail.com>
2025-09-07 17:48:51 +0200

    fix

--- app/api/leagues/route.js
+++ app/api/leagues/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
-import dbConnect from '../../../../lib/db/mongoose'
-import League from '../../../../lib/models/League'
-import City from '../../../../lib/models/City'
+import dbConnect from '../../../lib/db/mongoose'
+import League from '../../../lib/models/League'
+import City from '../../../lib/models/City'
 
 export const dynamic = 'force-dynamic'
 
